94375c9013f8d9490651b7724016966ae887f9c8
Uvais Ahmad <uvss21@gmail.com>
2024-12-14 01:03:33 +0530

    [Create ] First shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+"use client"
+import { Button } from '@/components/ui/button';
+import { useTheme } from 'next-themes';
+import React, { useState } from 'react';
 export default function Home() {
+  const {setTheme} = useTheme();
+  const [themeToggle, setThemeToggle] = useState<boolean>(false)
   return (
     <div className=''>
-      
+      hello world
+      <Button onClick={() => {setTheme(themeToggle? "dark": "light");setThemeToggle(!themeToggle)}}>Click me</Button>
     </div>
   );
 }
