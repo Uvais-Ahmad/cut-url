13d4cf48328676354bd0d21cae1c1d9247c69743
Uvais Ahmad <uvss21@gmail.com>
2024-12-13 23:51:30 +0530

    [Setup ] Tailwind based component library

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 export default function Home() {
   return (
-    <div className='flex justify-center mt-10 font-extrabold'>
-      Hello Uvais
+    <div className=''>
+      
     </div>
   );
 }
