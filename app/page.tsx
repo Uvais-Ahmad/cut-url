ba75bcc7ed53ef8d89b7e022b4ec38dbee3d5ede
Uvais Ahmad <uvss21@gmail.com>
2024-12-14 23:49:00 +0530

    [Remove] Unwanted Code

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client"
-import { Button } from '@/components/ui/button';
-import { useTheme } from 'next-themes';
-import React, { useState } from 'react';
+// import { Button } from '@/components/ui/button';
+// import { useTheme } from 'next-themes';
+import React from 'react';
 export default function Home() {
-  const {setTheme} = useTheme();
-  const [themeToggle, setThemeToggle] = useState<boolean>(false)
+  // const {setTheme} = useTheme();
+  // const [themeToggle, setThemeToggle] = useState<boolean>(false)
   return (
     <div>
       <h1 className='text-2xl md:text-4xl font-bold text-center'>
