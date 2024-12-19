ce855729c74a6dafc30e87d9635d26a7335243f1
Uvais Ahmad <uvss21@gmail.com>
2024-12-19 21:55:24 +0530

    [Update]

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div>
             <h1 className='text-2xl md:text-4xl font-bold text-center'>
-                {/* {`Shorten Your Loooong Links :)`} */}
+                
                 home page
             </h1>
         </div>
