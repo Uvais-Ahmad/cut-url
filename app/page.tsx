b422abb2c8f5eb5ecc08437c51c162a36f9808a2
Uvais Ahmad <uvss21@gmail.com>
2024-12-28 23:41:13 +0530

    [Updare]

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,11 @@ export default function Home() {
     <div className='h-full'>
       <NavBar />
       <div>
-        <h1 className='text-2xl md:text-4xl font-bold text-center'>
+        <h1 className='text-2xl  md:text-4xl font-bold text-center'>
           {`Shorten Your Loooong Links :)`}
         </h1>
         <p className='text-center mt-6'>CutLink is an efficiently and easy-to-use URL shortening service that streamlines your online experience.</p>
+        {/* Component which contain search bar */}
         <LinkShortenerInput />
       </div>
     </div>
