0566a7a52d5cd96ec119132d2d9d93e5423c9166
Uvais Ahmad <uvss21@gmail.com>
2025-01-03 00:03:00 +0530

    [Fix ] Issue of Responsive

--- components/LinkShortnerInput.tsx
+++ components/LinkShortnerInput.tsx
@@ -6,11 +6,11 @@ import { Label } from '@radix-ui/react-label'
 function LinkShortenerInput() {
     return (
         <div className='h-full'>
-            <div className='w-3/4  md:w-2/4 lg:w-2/5 mt-10 relative flex mx-auto'>
-                <Link className='absolute left-5 top-5 text-neutral-500' />
-                <input className='w-full p-4 rounded-full bg-neutral-900 border-4 pl-12' type='text' placeholder='Paste your link here' />
-                <button className='bg-blue-500 hidden sm:block w-30  absolute right-1 top-1 text-white p-4 px-6 rounded-full'>Shorten Now</button>
-                <button className='bg-blue-500 block sm:hidden w-15 absolute right-1 top-1 text-white p-4 px-6 rounded-full'>
+            <div className='w-3/4 mt-10 relative flex mx-auto  md:w-2/4 lg:w-2/5'>
+                <Link className='absolute text-neutral-500 left-4 top-4 sm:left-5 sm:top-5 ' />
+                <input className='w-full rounded-full bg-neutral-900 border-4 p-3 pl-12 sm:p-4 sm:pl-12' type='text' placeholder='Paste your link here' />
+                <button className='bg-blue-500 hidden w-30 px-6 rounded-full absolute right-1 top-1 text-white p-3 sm:block sm:p-4'>Shorten Now</button>
+                <button className='bg-blue-500 w-15 absolute right-1 top-1 text-white px-6 rounded-full p-3 block sm:hidden sm:p-4'>
                     <MoveRight/>
                 </button>
             </div>
