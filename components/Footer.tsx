57187462665ff225086f66f7014a106e6cf5cb68
Uvais Ahmad <uvss21@gmail.com>
2025-01-14 22:44:08 +0530

    [Fix ] UI Issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className='fixed bottom-0 w-full bg-gray-500 bg-opacity-10 h-16 backdrop-blur'>
             <div className='flex justify-center items-center py-4 px-4'>
-                <p className='text-sm'>© 2021 ShortLink. All rights reserved.</p>
+                <p className='text-sm'>© 2021 ShortLink. All rights reserved.</p> 
             </div>
         </div>
     )
