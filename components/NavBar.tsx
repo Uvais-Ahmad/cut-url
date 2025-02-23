8045173bd3cda20832ee5b8f47ae7c4af6f6b244
Uvais Ahmad <uvss21@gmail.com>
2025-02-23 23:24:57 +0530

    []

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,7 +9,7 @@ function NavBar() {
         <div className='flex justify-between items-center py-4 px-4'>
             <h1 className='text-2xl font-bold sm:font-extrabold tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-blue-700'>Briefly</h1>
             <div className='flex items-center gap-2'>
-{/* adding  */}
+
                 <Button
                     variant="ghost"
                     size="icon"
