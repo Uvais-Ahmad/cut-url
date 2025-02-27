c878b83b6a581a8cf7dac1d97e3da1bbb2eeb879
Uvais Ahmad <uvss21@gmail.com>
2025-02-28 00:46:53 +0530

    [CREATE] Decrypt and CreateSesssion services

--- lib/session.ts
+++ lib/session.ts
@@ -1,11 +1,38 @@
-import {JWTPayload, SignJWT} from 'jose'
+import {JWTPayload, jwtVerify, SignJWT} from 'jose'
+import { cookies } from 'next/headers';
 const secretKey = process.env.SESSION_SECRET
 const encryptKey = new TextEncoder().encode(secretKey);
 
-export async function encryptSession(payload: JWTPayload) {
+export async function encrypt(payload: JWTPayload) {
     return new SignJWT(payload)
         .setProtectedHeader({alg: 'HS256'})
         .setIssuedAt()
-        .setExpirationTime('24h')
+        .setExpirationTime('1d')
         .sign(encryptKey)
+}
+
+export async function decrypt(session: string | undefined = '') {
+    try {
+        const {payload } = await jwtVerify(session, encryptKey, {
+            algorithms: ['HS256']
+        })
+        return payload;
+    }
+    catch (e) {
+        console.error("Error in decrypt",e);
+    }
+}
+
+
+export async function createSession(userId: string) {
+    const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24);
+    const session = await encrypt({userId, expiresAt});
+    const cookieStore = await cookies();
+    cookieStore.set('session', session, {
+        httpOnly: true,
+        secure: true,
+        expires: expiresAt,
+        sameSite: 'lax',
+        path: '/'
+    });
 }
