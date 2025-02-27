8d62ba21a3eae5ed7694188f45ef3a6ec35a2ce8
Uvais Ahmad <uvss21@gmail.com>
2025-02-28 00:10:40 +0530

    [CREATE ] EncryptSession

--- lib/session.ts
+++ lib/session.ts
@@ -1,3 +1,11 @@
-export async function encryptSession(payload: SessionPayload) {
-    
+import {JWTPayload, SignJWT} from 'jose'
+const secretKey = process.env.SESSION_SECRET
+const encryptKey = new TextEncoder().encode(secretKey);
+
+export async function encryptSession(payload: JWTPayload) {
+    return new SignJWT(payload)
+        .setProtectedHeader({alg: 'HS256'})
+        .setIssuedAt()
+        .setExpirationTime('24h')
+        .sign(encryptKey)
 }
