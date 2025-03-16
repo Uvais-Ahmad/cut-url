3ce6b72bf4fe39da195225245e4b2bcd8705fafe
Uvais Ahmad <uvss21@gmail.com>
2025-03-16 23:48:39 +0530

    [Refactor] Register Authentication logic

--- actions/auth.ts
+++ actions/auth.ts
@@ -5,53 +5,6 @@ import { createSession, deleteSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 const prisma = new PrismaClient();
 
-// export async function register( state: TRegisterFormSchema, formData: FormData) {
-//     const validateFields = RegisterFormSchema.safeParse({
-//         name: formData.get('name') as string,
-//         email: formData.get('email') as string,
-//         password: formData.get('password') as string,
-//     });
-
-//     if(!validateFields?.success) {
-//         return {
-//             errors: validateFields.error.flatten().fieldErrors,
-//         }
-//     }
-
-//     const {name, email, password} = validateFields.data;
-//     const hashPassword = await bcrypt.hash(password, 10);
-//     console.log("name, email, password", name, email, password);
-
-//     const userExists = await prisma.user.findUnique({
-//         where: {
-//             email
-//         }
-//     });
-//     if(userExists) {
-//         return {
-//             errors: {
-//                 email: 'Email already exists',
-//                 name: '',
-//                 password: []
-//             }
-//         }
-//     }
-
-//     // Create user in database
-//     const user = await prisma.user.create({
-//         data: {
-//             name,
-//             email,
-//             password: hashPassword
-//         },
-//     });
-
-//     // create session
-//     await createSession(user?.id);
-//     // authorization
-//     redirect('/home');
-// }
-
 export async function logout () {
     await deleteSession()
     redirect('/login');
