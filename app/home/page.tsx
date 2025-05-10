e18f8a880a89600036347637b2fc20bc4edef266
Uvais Ahmad <uvss21@gmail.com>
2025-05-10 23:24:29 +0530

    [WIP] Home page creation starting

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,12 @@
+"use client"
+import NavBar from '@/components/NavBar'
 import React from 'react'
 
 // Home Page (for authenticated users)
 function MainHomePage() {
     return (
         <div className='h-full'>
-            
+            <NavBar />
         </div>
     )
 }
