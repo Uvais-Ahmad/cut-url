01f8f541df7f5515254b64432d1af3877708e022
Uvais Ahmad <uvss21@gmail.com>
2025-05-23 17:03:49 +0530

    [Update] Home page add table and buttons

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,104 @@
 "use client"
+import CopyToClipboard from '@/components/CopyToClipboard';
+import { DataTable } from '@/components/DataTable';
 import LinkShortenerInput from '@/components/LinkShortnerInput'
 import NavBar from '@/components/NavBar'
+import { Button } from '@/components/ui/button';
+import { TabsContent } from '@/components/ui/tabs';
+import { getShortUrl } from '@/lib/api';
+import { ShortUrlsProps } from '@/types';
 import { Tabs, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
-import { BarChart2, Clock, LinkIcon, Settings } from 'lucide-react';
-import React, { useState } from 'react'
+import { ColumnDef } from '@tanstack/react-table';
+import { BarChart2, Clock, Link, LinkIcon, ListFilterPlus, ListTodo, QrCode, Settings, Unlink } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
 
 // Home Page (for authenticated users)
 function MainHomePage() {
     const [reRender, setReRender] = useState<boolean>(false);
     const [activeTab, setActiveTab] = useState('history');
-    console.log("MainHomePage re-rendered", reRender);
+    
+    const [data, setData] = useState<ShortUrlsProps[]>([]);
+    const fetchShortUrl = async () => {
+        const response = await getShortUrl();
+        const data =  response?.data?.shortUrls as ShortUrlsProps[];
+        if(data.length) {
+            setData(data);
+        }   
+    }
+
+    useEffect(() => {
+        console.log('fetching data', reRender);
+        fetchShortUrl();  
+    }, [reRender]);
+
+  const column : ColumnDef<ShortUrlsProps>[] = [
+    {
+      accessorKey : 'shortUrl',
+      // header : 'Short URL',
+      header : () => <div className='ml-2 font-semibold'>Short URL</div>,
+      cell : ({row}) => {
+        const data = row.getValue('shortUrl') as string
+        return (
+          <div className='flex items-center'>
+            <div className='text-neutral-400'>{data}</div>
+            <CopyToClipboard>
+              {data}
+            </CopyToClipboard>
+          </div>
+        )
+      }
+    },
+    {
+      accessorKey : 'originalUrl',
+      
+      header : () => <div className='ml-6 font-semibold'>Original URL</div>,
+      cell : ({row}) => {
+        const data = row.getValue('originalUrl') as string;
+        return (
+          <div className='flex items-center'>
+            <Link className='text-blue-500 mx-2' size={'16px'} />
+            <p className='text-neutral-400 truncate md:w-48'>{data}</p>
+          </div>
+        )
+      }
+    },
+    {
+      accessorKey : 'qrCode',
+      header : 'QR Code',
+      cell : ({ }) => {
+        return (
+          // 
+          <div className='flex items-center'>
+            <QrCode className='text-neutral-400'/>
+          </div>
+          // 
+        )
+      }
+    },
+    {
+      accessorKey : 'clicks',
+      header : 'Clicks',
+    },
+    {
+      accessorKey : 'active',
+      header : 'Status',
+      cell : ({row}) => {
+        const data = row.getValue('active') as boolean;
+        return (
+          <div className='flex items-center'>
+            <div className={`rounded-full ml-2 p-1 ${data ? 'bg-green-900 bg-opacity-30 dark:bg-green-600 dark:bg-opacity-30' : 'bg-amber-900 bg-opacity-25 dark:bg-amber-600 dark:bg-opacity-25'}`}>
+              {data ? <Link className='dark:text-green-300 text-green-600 p-1'/> : <Unlink className='dark:text-amber-400 text-amber-600 p-1'/>}
+            </div>  
+            <div className={`ml-2 ${data ? 'dark:text-green-300 text-green-600': 'dark:text-amber-400 text-amber-600'}`}>{data ? 'Active' : 'Inactive'}</div>
+          </div>
+        )
+      }
+    },
+    {
+      accessorKey : 'createdAt',
+      header : 'Date',
+    }
+  ]
     return (
         <div className='h-full'>
             <NavBar />
@@ -21,8 +110,8 @@ function MainHomePage() {
                 {/* Tabs */}
                 <div className='min-h-screen '>
                     <div className='container mx-auto py-6'>
-                        <Tabs defaultValue="history" className="w-full bg-gray-50 dark:bg-neutral-900 shadow" onValueChange={setActiveTab}>
-                            <TabsList className="w-full flex justify-center bg-transparent p-0">
+                        <Tabs defaultValue="history" className="w-full" onValueChange={setActiveTab}>
+                            <TabsList className="w-full flex justify-center bg-transparent p-0 bg-gray-50 dark:bg-neutral-900 shadow">
                                 <TabsTrigger
                                     value="history"
                                     className={`flex items-center gap-2 rounded-none border-b-2 px-8 py-4 ${activeTab === "history" ? "border-blue-500 text-blue-500" : "border-transparent text-gray-400"}`}
@@ -52,6 +141,26 @@ function MainHomePage() {
                                     Settings
                                 </TabsTrigger>
                             </TabsList>
+
+                            <TabsContent value="history" className='w-2/3 mx-auto'>
+                                <div className='flex justify-between items-center'>
+                                    <h2>History (143)</h2>
+                                    <div className='flex justify-center items-center ml-auto'>
+                                        <Button variant='outline' className='mx-1'>
+                                            <ListTodo className='mr-2' />
+                                            Bulk Edit
+                                        </Button>
+                                        <Button variant='outline' className='mx-1'>
+                                            <ListFilterPlus className='mr-2' />
+                                            Filters
+                                        </Button>
+                                    </div>
+                                </div>
+
+                                <div className='w-full mt-4'>
+                                    <DataTable columns={column} data={data} />
+                                </div>
+                            </TabsContent>
                         </Tabs>
                     </div>
                 </div>
