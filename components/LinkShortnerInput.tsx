7e41d3e8afe912323a748702a2a2c4a6d582f210
Uvais Ahmad <uvss21@gmail.com>
2025-06-28 18:40:32 +0530

    [rEMOVE ] testing code

--- components/LinkShortnerInput.tsx
+++ components/LinkShortnerInput.tsx
@@ -45,12 +45,12 @@ function LinkShortenerInput({
                 <Label htmlFor="auto-paste" className='text-neutral-500 mx-4'>Auto Paste from Clipboard</Label>
             </div> */}
 
-            {shortUrl || true && (
+            {shortUrl && (
                 <div className='flex items-center justify-center mt-6 dark:bg-neutral-900 border-1 w-1/5 mx-auto rounded-lg p-2 px-1'>
                     <div className='flex items-center justify-between w-full'>
                         <div className='flex items-center'>
                             <div className='w-2 h-2 bg-green-500 rounded-full mr-2'></div>
-                            <div className='text-neutral-400'>{shortUrl || 'http://localhost:3000/srpxYUU'}</div>
+                            <div className='text-neutral-400'>{shortUrl}</div>
                         </div>
                         <Button size={'icon'} className='bg-gray-800 text-white rounded-full hover:bg-gray-700'>
                             <Copy />
